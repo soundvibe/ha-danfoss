48cb858d43047d44c51dfe7c6bb1608e47f22431
soundvibe <linas.n@chronosphere.io>
2024-01-13 00:03:16 +0200

    wip

--- danfoss-addon/src/main/java/net/soundvibe/hasio/HomeAssistantClient.java
+++ danfoss-addon/src/main/java/net/soundvibe/hasio/HomeAssistantClient.java
@@ -4,13 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.soundvibe.hasio.danfoss.data.IconRoom;
 import net.soundvibe.hasio.data.State;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Map;
 
 public class HomeAssistantClient {
@@ -19,17 +16,14 @@ public class HomeAssistantClient {
 
     private final String endpoint;
     private final String token;
-    private final HttpClient httpClient;
+    private final OkHttpClient httpClient;
 
     private final Gson gson = new GsonBuilder().create();
 
     public HomeAssistantClient(String endpoint, String token) {
         this.endpoint = endpoint;
         this.token = token;
-        this.httpClient = HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.ALWAYS)
-                .version(HttpClient.Version.HTTP_1_1)
-                .build();
+        this.httpClient = new OkHttpClient.Builder().build();
     }
 
     public void upsertRoomThermostat(IconRoom room) {
@@ -52,20 +46,23 @@ public class HomeAssistantClient {
         var jsonString = this.gson.toJson(sensorState);
 
         try {
+            var body = RequestBody.create(jsonString, MediaType.parse("application/json"));
             // temperature
-            var response = this.httpClient.send(HttpRequest.newBuilder()
-                            .uri(URI.create(String.format("%s/states/%s", this.endpoint, sensorName)))
-                            .header("Authorization", String.format("Bearer %s", this.token))
-                            .header("Content-Type", "application/json")
-                            .POST(HttpRequest.BodyPublishers.ofString(jsonString))
-                            .build(),
-                    HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                return;
+            var request = new Request.Builder()
+                    .url(String.format("%s/states/%s", this.endpoint, sensorName))
+                    .header("Authorization", String.format("Bearer %s", this.token))
+                    .header("Content-Type", "application/json")
+                    .post(body)
+                    .build();
+
+            Call call = this.httpClient.newCall(request);
+            try (var response = call.execute()) {
+                if (response.code() == 200 || response.code() == 201) {
+                    return;
+                }
+
+                logger.error("[{}]: failed to upsert sensor: {}", response.code(), response.message());
             }
-
-            logger.error("[{}]: failed to upsert sensor: {}", response.statusCode(), response.body());
-
         } catch (Exception e) {
             logger.error("unable to upsert Home Assistant states", e);
             throw new RuntimeException(e);
