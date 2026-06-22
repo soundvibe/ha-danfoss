b7995f26272a275717a926c1c75db8533f1d76dd
soundvibe <lnaginionis@gmail.com>
2026-06-22 14:12:46 +0300

    fix potential NPE in IconMasterTest add maven wrapper

--- danfoss-addon/src/test/java/net/soundvibe/hasio/danfoss/data/IconMasterTest.java
+++ danfoss-addon/src/test/java/net/soundvibe/hasio/danfoss/data/IconMasterTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class IconMasterTest {
 
@@ -15,7 +18,21 @@ class IconMasterTest {
         var sut = new IconMaster("Test House", 17.0, 21.0, "1.0", "21.0", "0",
                 1, 1, Instant.parse("2023-12-03T10:15:30.00Z"));
         var state = sut.toState();
-        Assertions.assertNotNull(state);
+        assertNotNull(state);
+    }
+
+    @Test
+    void test_state_json_with_null_fields() {
+        var sut = new IconMaster(null, 0.0, 0.0, null, null, null,
+                0, 0, null);
+        var state = sut.toState();
+        assertNotNull(state);
+        var attributes = state.attributes();
+        assertEquals("null", attributes.get("house_name"));
+        assertEquals("null", attributes.get("hardware_revision"));
+        assertEquals("null", attributes.get("software_revision"));
+        assertEquals("null", attributes.get("serial_number"));
+        assertEquals("null", attributes.get("production_date"));
     }
 
 }
